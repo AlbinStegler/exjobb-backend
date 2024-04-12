912fa191120614bf6ba505ceaf926334e4f22a3e
Albin Stegler <albin.stegler@gmail.com>
2024-04-12 13:36:58 +0200

    changed from jade to pug

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
