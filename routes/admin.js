cfe5d3c9a313b628668d815189cf86d4304e8830
Albin Stegler <albin.stegler@gmail.com>
2024-04-13 15:21:45 +0200

    admin path changed

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const admin = require('../models/admin');
 /* GET users listing. */
-router.post('/', (req, res) => admin.checkCredentials(req, res));
+router.post('/check', (req, res) => admin.checkCredentials(req, res));
 router.post('/', (req, res) => admin.createAdmin(req, res));
 
 module.exports = router;
